c6aba89f064fe0ca22679f8aaded56318e97fd76
Gaetan <150467427+GaetanAL@users.noreply.github.com>
2024-09-19 00:02:48 -0400

    partie 3

--- game.py
+++ game.py
@@ -123,12 +123,17 @@ class Game:
     def check_collision(self, direction):
         pass
         # TODO: Extraire les coordonnées de déplacement de la direction (dx, dy)
-
+        dx = direction[0]
+        dy = direction[1]
         # TODO: Calculer la nouvelle position de Pac-Man après le déplacement (new_x, new_y) avec la formule new_x = self.pacman.x + dx
-    
+        new_x = self.pacman.x + dx
+        new_y = self.pacman.y + dy
         # TODO: Vérifier si la nouvelle position est un chemin valide ou un mur
         # Utiliser la grille (`self.board`) pour déterminer si la case est un chemin (0) ou un mur (1). return True si c'est un chemin, False si c'est un mur.
-
+        if self.board[new_x][new_y] == 1:
+            return True
+        else:
+            return False
     def update(self):
         for ghost in self.ghosts:
             ghost.draw()
@@ -146,25 +151,28 @@ class Game:
 
     def check_score(self):
         # TODO: Vérifier si la position actuelle de Pac-Man (en coordonnées de grille) correspond à une position de pièce en utilisant (self.pacman.x, self.pacman.y)
-            # TODO: Si Pac-Man est sur une pièce, la retirer de la liste des pièces restantes à collecter 
-
+            # TODO: Si Pac-Man est sur une pièce, la retirer de la liste des pièces restantes à collecter  
+            if (self.pacman.x,self.pacman.y) in self.coins:
+                self.coins.remove((self.pacman.x,self.pacman.y))
             # TODO: Ajouter des points au score du joueur pour la pièce collectée (par exemple, 10 points)
-
-        if len(self.coins) == 0:
-            self.end.render(True)
-            self.game_won = True
-            self.game_over = True
+                self.score += 10
+            if len(self.coins) == 0:
+                self.end.render(True)
+                self.game_won = True
+                self.game_over = True
 
     def check_special_coins(self):
         pass
         # TODO: Vérifier si la position actuelle de Pac-Man (en coordonnées de grille) correspond à une position de pièce spéciale
-
+        
+        if (self.pacman.x,self.pacman.y) in self.special_coins:
+                    
             # TODO: Si Pac-Man est sur une pièce spéciale, retirer cette pièce spéciale de la liste
-            
+                self.special_coins.remove((self.pacman.x,self.pacman.y))
             # TODO: Ajouter des points au score du joueur pour la pièce spéciale collectée
-            
+                self.score += 50
             # TODO: Activer le mode "manger" en appelant la méthode appropriée pour activer le mode "manger" des fantômes avec self.activate_eat_mode()
-
+                self.activate_eat_mode()
 
     def activate_eat_mode(self):
         timer = threading.Timer(EDIBLE_GHOST_TIMER, self.deactivate_eat_mode)
